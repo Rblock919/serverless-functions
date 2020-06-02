3131a8cee0fa4de64b41ff23d9ada4a80b590356
Ryan Block <ryan.block93@gmail.com>
2020-06-01 22:53:13 -0400

    created openfaas functions for grabbing charlottes weather and returning the passed in context object for the node12-armhf template

---
+++ open-faas/grab-weather-armhf/handler.js
@@ -0,0 +1,16 @@
+"use strict";
+const rp = require("request-promise-native");
+
+const options = {
+  method: "GET",
+  uri: "https://www.metaweather.com/api/location/2378426/",
+  headers: {
+    "content-type": "application/json",
+  },
+  json: true, // Automatically stringifies the body to JSON
+};
+
+module.exports = async (context, callback) => {
+  const weather = await rp(options);
+  return { weather };
+};
